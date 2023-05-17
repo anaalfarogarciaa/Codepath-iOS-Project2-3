7ff8d3cbcae134fa81d09974826e6993772e423e
Ana Alfaro-Garcia <73010333+anaalfarogarciaa@users.noreply.github.com>
2023-05-16 23:20:31 -0700

    Added files

---
+++ Codepath-IOS-Flixster-main/IOS_Project_2_Flixster/IOS_Project_2_Flixster/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  IOS_Project_2_Flixster
+//
+//  Created by Ana Alfaro-Garcia on 05/15/23.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
